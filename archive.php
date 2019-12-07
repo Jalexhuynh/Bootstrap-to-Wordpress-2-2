66616373b84abeba334eafffc8189f4322185f7b
Justin Huynh <j.alexhuynh@gmail.com>
2019-12-07 14:55:29 -0800

    Correct markup for archive pages.

--- archive.php
+++ archive.php
@@ -10,17 +10,16 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<?php if ( have_posts() ) : ?>
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1 class="page-title"><?php the_archive_title('', $the_archive_description); ?>
+			<small class="archive-description"><?php the_archive_description(); ?></small>	
+		</h1>
+	</section>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+<div class="container">
+	<div id="primary" class="row">
+		<main id="content" class="col-sm-8">
 
 			<?php
 			/* Start the Loop */
@@ -28,26 +27,31 @@ get_header();
 				the_post();
 
 				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
+					* Include the Post-Type-specific template for the content.
+					* If you want to override this in a child theme, then include a file
+					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					*/
 				get_template_part( 'template-parts/content', get_post_type() );
 
 			endwhile;
 
 			the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
+			endif;
+			?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main> <!-- end #content -->
 
-<?php
-get_sidebar();
-get_footer();
+		<!-- Sidebar -->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</aside>
+
+	</div> <!-- end #primary -->
+</div> <!-- end container -->
+
+<?php get_footer(); ?>
