69d050e86f2473ad700344649cac4db5af4b247c
Justin Huynh <j.alexhuynh@gmail.com>
2019-12-07 15:29:58 -0800

    Fix header for fullwidth template and create gitignore file.

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -1,14 +1,27 @@
 <?php 
 /* Template Name: Full Width Template */
 
+$thumbnail_url          = wp_get_attachment_url( get_post_thumbnail_id( $post ) );
+
 get_header();
 ?>
 
 <!-- FEATURE IMAGE
 ================================================== -->
-<section class="feature-image feature-image-default" data-type="background" data-speed="2">
-    <h1>Contact</h1>
-</section>
+
+<?php if( has_post_thumbnail() ) { ?>
+    
+    <section class="feature-image" style="background: url('<?php echo $thumbnail_url?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
+        <h1><?php the_title(); ?></h1>
+    </section>
+
+<?php } else { ?>
+
+    <section class="feature-image feature-image-default" data-type="background" data-speed="2">
+        <h1><?php the_title(); ?></h1>
+    </section>
+
+<?php } ?>
 
 <!-- MAIN CONTENT
 ================================================== -->
@@ -18,34 +31,13 @@ get_header();
         <div id="content" class="col-sm-12">
             
             <section class="main-content">
-                <p class="lead">Have any questions about the course? Feel free to get in touch with me! I&rsquo;ll do my best to get back to you ASAP.</p>
+
+                <?php while ( have_posts() ) : the_post();
                 
-                <form role="form" class="clearfix">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="sr-only" for="contact-name">Name</label>
-                                <input type="text" class="form-control input-lg" id="contact-name" placeholder="Your name">
-                            </div>
-                        </div><!-- end col -->
-                        
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="sr-only" for="contact-email">Email</label>
-                                <input type="text" class="form-control input-lg" id="contact-email" placeholder="Your email">
-                            </div>
-                        </div><!-- end col -->
-                        
-                        <div class="col-sm-12">
-                            <div class="form-group">
-                                <label class="sr-only" for="contact-words">Message</label>
-                                <textarea class="form-conrol input-lg" id="contact-words" placeholder="Your message" rows="3"></textarea>
-                            </div>
-                        </div><!-- end col -->
-                    </div><!-- row -->
-                    <input type="submit" class="btn btn-info btn-lg pull-right" value="Get in touch &raquo;">
-                </form>
-                
+                    the_content();
+
+                endwhile; ?>
+
             </section>
             
         </div><!-- content -->
